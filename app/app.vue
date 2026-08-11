c9d9e86c1dbcfdab7936c97d3c443b2385dbfa45
maxblom123 <maxacblom@proton.me>
2026-08-11 16:08:39 +0200

    footer header

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { AppHeader, AppFooter } from '@maxblom/headlessmax-appshell'
+</script>
+
+<template>
+  <AppHeader title="maxshelf" />
+  <main style="padding: 2rem;">
+    <h1>maxshelf</h1>
+    <p>If you can see this and the header/footer, HeadlessMax + AppShell are wired up.</p>
+  </main>
+  <AppFooter />
+</template>
